7f7eff048f62d7fe318aabe547e0f0851ba38fb4
fffrowies <fernandorowies.saxo@gmail.com>
2019-01-31 14:17:52 -0300

    Start tracking orders, show google map (default location)

--- app/src/main/java/com/fffrowies/sbadminserver/Common/Common.java
+++ app/src/main/java/com/fffrowies/sbadminserver/Common/Common.java
@@ -1,9 +1,11 @@
 package com.fffrowies.sbadminserver.Common;
 
+import com.fffrowies.sbadminserver.Model.Request;
 import com.fffrowies.sbadminserver.Model.User;
 
 public class Common {
     public static User currentUser;
+    public static Request currentRequest;
 
     public static final String UPDATE = "Update";
     public static final String DELETE = "Delete";
--- app/src/main/java/com/fffrowies/sbadminserver/OrderStatus.java
+++ app/src/main/java/com/fffrowies/sbadminserver/OrderStatus.java
@@ -1,6 +1,7 @@
 package com.fffrowies.sbadminserver;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -57,7 +58,7 @@ public class OrderStatus extends AppCompatActivity {
                 requests
         ) {
             @Override
-            protected void populateViewHolder(OrderViewHolder viewHolder, Request model, int position) {
+            protected void populateViewHolder(OrderViewHolder viewHolder, final Request model, int position) {
 
                 viewHolder.txvOrderId.setText(adapter.getRef(position).getKey());
                 viewHolder.txvOrderStatus.setText(Common.convertCodeToStatus(model.getStatus()));
@@ -67,7 +68,9 @@ public class OrderStatus extends AppCompatActivity {
                 viewHolder.setItemClickListener(new ItemClickListener() {
                     @Override
                     public void onClick(View view, int position, boolean isLongClick) {
-                        //Just implemented to fix crash when click this item
+                        Intent trackingOrder = new Intent(OrderStatus.this, TrackingOrder.class);
+                        Common.currentRequest = model;
+                        startActivity(trackingOrder);
                     }
                 });
             }
